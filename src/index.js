4dd3dd64e91e02c6446c6ce712e73e7e4b0ebe7b
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-05-08 14:38:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './sass/main.scss';
+import colors from '../colors.json';
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action = start]'),
+  stopBtn: document.querySelector('button[data-action = stop]'),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  toggleDisabledAttribute();
+  bodyColorSwitcher();
+
+  timerId = setInterval(() => {
+    bodyColorSwitcher();
+  }, 1000);
+});
+
+
+refs.stopBtn.addEventListener('click', () => {
+  toggleDisabledAttribute();
+  clearInterval(timerId);
+});
+
+function bodyColorSwitcher() {
+  const randomColor = randomIntegerFromInterval(0, colors.length - 1);
+  refs.body.style.backgroundColor = colors[randomColor];
+};
+
+function toggleDisabledAttribute() {
+  refs.startBtn.toggleAttribute('disabled');
+  refs.stopBtn.toggleAttribute('disabled');  
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
